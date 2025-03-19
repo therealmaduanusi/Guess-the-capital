16d3f02c539d74403ac4724d280f99a9cd6ddfa2
Victor Maduanusi <maduanusivictor2020@gmail.com>
2025-03-19 16:47:13 +0100

    db connection

--- index.js
+++ index.js
@@ -3,62 +3,79 @@ import bodyParser from "body-parser";
 import pg from "pg";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
+import env from "dotenv";
 
+env.config();
+
+const db = new pg.Client({
+  user: process.env.USER_NAME,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: process.env.PORT,
+});
+
+db.connect()
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
-const port = 3001
+const port = 3001;
+
+// quiz array
+let quiz = [];
+db.query("SELECT * FROM capital", (err, res) => {
+    if (err) {
+      console.error("Error executing query", err.stack);
+    } else {
+      quiz = res.rows;
+    }
+    db.end();
+});
 
 // Middleware
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
 // Set EJS as the view engine
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 
-// quiz array
-let quiz = [
-    { country: "France", capital: "Paris" },
-    { country: "United Kingdom", capital: "London" },
-    { country: "United States of America", capital: "New York" },
-]
 
 let currentQuestion;
 let totalCorrect = 0;
 
 // route
 app.get("/", (req, res) => {
-    totalCorrect = 0;
-    nextQuestion()
-    res.render("index.ejs", {
-        question: currentQuestion
-    });
-})
+  totalCorrect = 0;
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+  });
+});
 
 app.post("/submit", (req, res) => {
-    let answer = req.body.answer.trim()
-    let isCorrect = false;
+  let answer = req.body.answer.trim();
+  let isCorrect = false;
 
-    if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
-        totalCorrect++
-        isCorrect = true;
-    }
+  if (currentQuestion.capital.toLowerCase() === answer.toLowerCase()) {
+    totalCorrect++;
+    isCorrect = true;
+  }
 
-    nextQuestion()
-    res.render("index.ejs", {
-        question: currentQuestion,
-        totalScore: totalCorrect,
-        wasCorrect: isCorrect
-    })
-})
+  nextQuestion();
+  res.render("index.ejs", {
+    question: currentQuestion,
+    totalScore: totalCorrect,
+    wasCorrect: isCorrect,
+  });
+});
 
 // random question
 function nextQuestion() {
-    const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
-    currentQuestion = randomCountry;
+  const randomCountry = quiz[Math.floor(Math.random() * quiz.length)];
+  currentQuestion = randomCountry;
 }
 
 app.listen(port, () => {
-    console.log(`Listening at port ${port}`);
-})
+  console.log(`Listening at port ${port}`);
+});
